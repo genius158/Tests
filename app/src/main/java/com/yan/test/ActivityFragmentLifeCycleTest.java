4320b2c5ad25d8dc001f751c119ba0536b42c6e0
yan <yanxianwei1114@gmail.com>
2017-12-26 19:34:57 +0800

    replace fragment life cycle test

--- app/src/main/java/com/yan/test/ActivityFragmentLifeCycleTest.java
+++ app/src/main/java/com/yan/test/ActivityFragmentLifeCycleTest.java
@@ -2,6 +2,7 @@ package com.yan.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ActivityFragmentLifeCycleTest extends AppCompatActivity {
 
@@ -12,5 +13,13 @@ public class ActivityFragmentLifeCycleTest extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction().add(R.id.fl, LifeCycleFragment.inner()).commit();
+
+        findViewById(R.id.fl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl,new ViewPagerFragment()).commit();
+            }
+        });
+
     }
 }
